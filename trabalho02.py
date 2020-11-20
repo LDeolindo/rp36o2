a569c5d7a544f0949d792f89adab02e1386aecf9
LDeolindo <lucas.deolindo1413@gmail.com>
2020-11-20 13:40:25 -0300

    Finishedgit add *

---
+++ trabalho02.py
@@ -0,0 +1,74 @@
+from pylsl import StreamInlet, resolve_stream
+import time
+import numpy as np
+import scipy
+from scipy.signal import detrend
+import mne
+
+def findEvents(data):
+  f = open('rp360-30.txt', 'a', encoding="utf8")
+  bandPower = np.array([0, 0, 0, 0])
+
+  # delta, _ = mne.time_frequency.psd_welch(data, n_per_seg=250 ,fmin=0.5, fmax=4)
+  theta, _ = mne.time_frequency.psd_welch(data, n_per_seg=250 ,fmin=4, fmax=8)
+  alfa, _ = mne.time_frequency.psd_welch(data, n_per_seg=250 ,fmin=8, fmax=13)
+  beta, _ = mne.time_frequency.psd_welch(data, n_per_seg=250 ,fmin=13, fmax=32)
+  gamma, _ = mne.time_frequency.psd_welch(data, n_per_seg=250 ,fmin=32, fmax=100)
+
+  # bandPower[0] = np.average(delta)
+  bandPower[0] = np.average(theta)
+  bandPower[1] = np.average(alfa)
+  bandPower[2] = np.average(beta)
+  bandPower[3] = np.average(gamma)
+
+  # returns an array with the index of the 2 highest values
+  maxValueIdx = (-bandPower).argsort()[:2]
+
+  # checks if the Alpha has the highest value
+  if (maxValueIdx[0] == 1):
+    maxValue = bandPower[maxValueIdx[0]]
+    minValue = bandPower[maxValueIdx[1]]
+    f.write('\n Ritmos Alpha \n')
+    f.write(str(minValue*100/maxValue))
+  # checks if the Beta has the highest value
+  elif (maxValueIdx[0] == 2):
+    maxValue = bandPower[maxValueIdx[0]]
+    minValue = bandPower[maxValueIdx[1]]
+    f.write('\n Ritmos Beta \n')
+    f.write(str(minValue*100/maxValue))
+  else:
+    f.write('0 \n')
+
+def main():
+  sample_rate = 250
+  # info = mne.create_info(ch_names=['O1', 'O2', 'P3', 'Pz', 'P4', 'C3', 'Cz', 'C4'], sfreq=sample_rate, ch_types='eeg')
+  info = mne.create_info(ch_names=['1', '2', '3', '4', '5', '6', '7', '8'], sfreq=sample_rate, ch_types='eeg')
+
+  print("Looking for an EEG stream...")
+  streams = resolve_stream('type', 'EEG')
+
+  # create a new inlet to read from the stream
+  inlet = StreamInlet(streams[0])
+
+  while True:
+    # sample1, _ = inlet.pull_chunk(timeout=8, max_samples=250) #
+    sample1, _ = inlet.pull_chunk(timeout=1, max_samples=250) # timeout=3 / timeout=1.5
+    # sample2, _ = inlet.pull_chunk(timeout=2.7, max_samples=250) #
+    # sample3, _ = inlet.pull_chunk(timeout=2.7, max_samples=250) #
+
+    sample1 = np.array(sample1)
+    # sample2 = np.array(sample2)
+    # sample3 = np.array(sample3)
+
+    # sample1 = np.concatenate((sample1, sample2, sample3))
+    sample = np.transpose(sample1)
+
+    raw = mne.io.RawArray(sample, info)
+    bandPass = raw.filter(l_freq=1, h_freq=50)
+    bandPass = bandPass.filter(l_freq=1, h_freq=50)
+    bandPass = bandPass.filter(l_freq=1, h_freq=50)
+    bandPass = bandPass.filter(l_freq=1, h_freq=50)
+    findEvents(bandPass)
+
+if __name__ == "__main__":
+  main()
